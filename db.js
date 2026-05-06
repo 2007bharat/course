0d9a3aad183b7a2eafd42a32fdfab6bd78d6ae8c
Bharat <bandanupretibisht@gmail.com>
2026-05-06 13:19:58 +0530

    create a db.js model

---
+++ db.js
@@ -0,0 +1,34 @@
+const mongoose =  require("mongoose");
+const {Schema} = mongoose;
+const { ObjectId }  = mongoose.Schema.Types;
+
+const userSchema =  new Schema({
+    username :  String,
+    email : {type  : String , unique : true},
+    password :  String
+})
+const adminSchema =  new Schema({
+    username :  String,
+    email : {type  : String , unique : true},
+    password :  String
+})
+
+const purchaseSchema = new Schema({
+    courseId :  {type :  ObjectId , ref : "Course"},
+    userId :  {type :  ObjectId , ref : "User"}
+})
+
+const courseSchema =  new Schema({
+    creatorId :  {type :  ObjectId , ref : "Admin"},
+    price : String,
+    img :  String,
+    title   :String,
+    subtitle : String
+
+})
+
+
+const userModel  =  mongoose.model("User", userSchema)
+const adminModel  =  mongoose.model("admin", adminSchema)
+const puchaseModel  =  mongoose.model("purchase", purchaseSchema)
+const courseModel  =  mongoose.model("Course", courseSchema)
